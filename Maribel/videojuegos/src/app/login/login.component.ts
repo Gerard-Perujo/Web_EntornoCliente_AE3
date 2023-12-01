103a18a9c5f84389828b9b697b855067b6ed7523
María Isabel <contacto.mims@gmail.com>
2023-12-01 18:16:55 +0100

    login terminado

--- Maribel/videojuegos/src/app/login/login.component.ts
+++ Maribel/videojuegos/src/app/login/login.component.ts
@@ -1,8 +1,10 @@
-//Se tienen que importar el Router y el usuario para poder obtener información de ellos.
+//Se tienen que importar el Component y el OnInit para generar el componente y su ciclo de vida;
+//el Router para la navegación entre páginas, y la clase creada para manejar la autetificación.
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { Usuario } from 'src/app/Clase_Usuario/Usuario';
+import { Autentificacion } from '../Clase_Autentificación/Autentificacion';
 
+//Esto se genera automáticamente, siendo los metadatos del componente, en concreto su selecto y las rutas a su HTML y CSS.
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -10,57 +12,33 @@ import { Usuario } from 'src/app/Clase_Usuario/Usuario';
 })
 
 /*
-Se van a inicializar los atributos que serán necesarios para ejecutar correctamente el login.
-Ahí mismo también se crea una lista de usuarios con sus nombres de usuario y respectivas contraseñas,
-para que se puedan consultar en cualquier momento.
-Luego necesitamos el mapa de la web que nos ayude a movernos de una página a otra: router.
+La clase del componente de iniciar sesión implementa la interfaz de ciclo de vida de dicho componenente.
+Se inicializan a su valor por defecto el nombre de las propiedades del componente.
 
-Posteriormente, se deben realizar dos métodos.
-El primero de ellos verifica la existencia del usuario. Recorre con un for toda la lista de usuarios.
-Si el nombre y la contraseñan que introduzcan es igual a usuario creado con el mismo nombre y contraseña,
-entonces devuelve true. Si no hay ningún usuario con los mismos valores de parámetros devuelve false.
+A continuación se inyectan las dependencias del componente en el constructor. Ambos son servicios:
+el primero para verificar usuarios y contraseñas, y el segundo para la navegabilidad. Su inyección
+permite emplear su funcionalidad.
 
-Finalmente, el método loguearse, en función del resultado que aporte el método verificar, 
-nos redirige al index, logrando iniciar sesión, y se guardan datos en un objeto que llamamos usuario
-que tiene el nombre del user para intercambiar datos entre páginas; 
-pero si devolvió false entonces salta el mensaje de error y permanecemos en la página
-
-Se necesitan los this para apuntar a objetos específicos de la instancia de esta clase.
+Seguidamente, el método controla la lógica del login.
+Si la verificación de la clase Autentificación (a la que se le pasa el user y la contraseña) es true,
+entonces somos redirigidos al index gracias a la navegación del router.
+En cambio, si devuelve nulo figurará el mensaje de error almacenado en this.incorrecto.
 */
-export class LoginComponent implements OnInit {
-  user: string = "";
-  contrasena: string = "";
-  mensaje : string = "";
-  usuariosExistentes: Usuario[] = [
-    new Usuario('Bowser', 'Peaches'),
-    new Usuario('Mario', 'its a me'),
-    new Usuario('Pikachu', 'te elijo a ti'),
-    new Usuario('Tiny', 'kong'),
-    new Usuario('Game', 'over')
-  ];
+export class LoginComponent implements OnInit{
+  user: string ="";
+  contrasena: string ="";
+  incorrecto = '';
 
-  constructor(private ruta: Router) {
+  constructor(private Autentificacion: Autentificacion, private router: Router) {}
 
-  }
-
-  public verificar (user: string, contrasena : string): boolean{
-    for (let usuario of this.usuariosExistentes){
-      if (usuario.user === user && usuario.contrasena ===contrasena){
-        return true;
-      } 
-    }
-    return false;
-  }
-
-  public loguearse(): void{
-    if (this.verificar(this.user, this.contrasena)) {
-      this.ruta.navigate(['/index', {usuario:this.user}]);
+  login() {
+    if (this.Autentificacion.verificar(this.user, this.contrasena)) {
+      this.router.navigate(['/index']);
     } else {
-      this.mensaje = "¡Vaya! Parece que el nombre estrella o la clave mágica no son los correctos.¡Anímate, aventurero del Mundo de los Sueños, e inténtalo de nuevo!";
+      this.incorrecto = '⚡️¡Vaya! Parece que el nombre estrella o la clave mágica no son los correctos.¡Anímate, aventurero del Mundo de los Sueños, e inténtalo de nuevo!'; 
     }
   }
 
   ngOnInit(): void {
   }
-
 }
